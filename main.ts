5356cbbcc40f5f75bd3914c0cc092457e20a4be9
esdraslov <103470150+esdraslov@users.noreply.github.com>
2023-02-19 22:14:30 -0300

    Atualizar 'main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -5,15 +5,22 @@ let DirectionR = false
 input.onButtonPressed(Button.A, function () {
     aperta_botão("esquerdo")
 })
+input.onButtonPressed(Button.B, function () {
+    aperta_botão("direito")
+})
 function aperta_botão (buttonDirection: string) {
     button = buttonDirection
     Direction2 = true
     if (buttonDirection.includes("esquerdo")) {
         DirectionL = true
         servos.P0.setAngle(90)
+        basic.pause(1000)
+        servos.P0.setAngle(0)
     } else {
         DirectionR = true
         servos.P1.setAngle(90)
+        basic.pause(1000)
+        servos.P1.setAngle(0)
     }
-    return Direction2
+    return "" + Direction2 + " " + button
 }
